2a145061d51cb96976992ed67d916d63d3621a41
ehesp <elliot.hesp@gmail.com>
2020-11-19 13:43:40 +0000

    Use buble alias

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   webpack: (config) => {
     
+    // https://github.com/FormidableLabs/react-live#what-bundle-size-can-i-expect
+    config.resolve.alias['buble'] = '@philpl/buble';
+
+    // TODO: remove if no domain loading is required locally
     config.module.rules.push({
       test: /\.txt$/,
       use: "raw-loader",
