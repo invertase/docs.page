b621a3c11199bfd6d0a72957af35bddf260965fd
ehesp <elliot.hesp@gmail.com>
2020-12-09 12:12:50 +0000

    tweak JumpToTop

--- src/components/JumpToTop.tsx
+++ src/components/JumpToTop.tsx
@@ -5,7 +5,7 @@ import { ChevronDoubleUp } from './Icons';
 
 const THRESHOLD = 300;
 
-export function JumpToTop() {
+function useJumpToTop(): boolean {
   const [visible, setVisible] = useState<boolean>(false);
   const y = useWindowScroll();
 
@@ -18,6 +18,12 @@ export function JumpToTop() {
       setVisible(false);
     }
   }, [visible, y]);
+
+  return visible;
+}
+
+export function JumpToTop() {
+  const visible = useJumpToTop();
 
   function goToTop() {
     if (typeof window === 'undefined') {
@@ -34,7 +40,7 @@ export function JumpToTop() {
   return (
     <div
       className={cx(
-        'fixed w-14 h-14 rounded-full right-0 bottom-0 mr-4 mb-4 cursor-pointer bg-theme-color shadow hover:shadow-xl transition-opacity',
+        'fixed w-12 h-12 rounded-full right-0 bottom-0 mr-4 mb-4 cursor-pointer bg-theme-color shadow hover:shadow-xl transition-opacity',
         {
           'opacity-0 pointer-events-none': !visible,
           'opacity-100 pointer-events-auto': visible,
@@ -46,7 +52,7 @@ export function JumpToTop() {
         onClick={goToTop}
         className="z-20 absolute inset-0 flex items-center justify-center"
       >
-        <ChevronDoubleUp size={26} className="text-white" />
+        <ChevronDoubleUp size={24} className="text-white" />
       </div>
     </div>
   );
