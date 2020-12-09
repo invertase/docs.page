c5c015a28a0ccd4d2f4209082a23d54cd699f2f6
ehesp <elliot.hesp@gmail.com>
2020-12-09 13:13:13 +0000

    fix JSX node sanitization

--- remark-sanitize-jsx.js
+++ remark-sanitize-jsx.js
@@ -1,7 +1,8 @@
 const sanitizeHtml = require('sanitize-html');
 const visit = require('unist-util-visit');
+const isHtml = require('is-html');
 
-// If a user provides HTML to a remote markdown file, it needs to 
+// If a user provides HTML to a remote markdown file, it needs to
 // be valid markup (e.g. closing tags) to be rendered as JSX.
 // Using the `sanitize-html` library, we can cleanup any jsx nodes
 // found during serialization.
@@ -10,10 +11,14 @@ function closeTags() {
     visit(tree, 'jsx', visitor);
   };
 
-  function visitor(node, index, parent) {
-    node.value = sanitizeHtml(node.value, {
-      allowedTags: [...sanitizeHtml.defaults.allowedTags, 'img'],
-    });
+  function visitor(node) {
+    // A `jsx` node might be a HTML element from the Markdown, or a MDX
+    // component. We only want to sanitize the HTML ones.
+    if (isHtml(node.value)) {
+      node.value = sanitizeHtml(node.value, {
+        allowedTags: [...sanitizeHtml.defaults.allowedTags, 'img'],
+      });
+    }
   }
 }
 
