d0fb21706ae183519e640be8d4d2664dd352dfbb
ehesp <elliot.hesp@gmail.com>
2021-06-18 08:19:57 +0100

    stub redirects logic

--- next.config.js
+++ next.config.js
@@ -1,23 +1,41 @@
 const fs = require('fs');
 const path = require('path');
 
+// Extract an array of domains and repositories
+const domains = fs
+  .readFileSync(path.join(process.cwd(), 'domains.txt'), 'utf-8')
+  .split('\n')
+  .map(line => line.split(' '))
+  .filter(line => line.length === 2);
+
 module.exports = {
   async redirects() {
+    // TODO: Not sure if this will work - since the pages wont be rendered because of the rewrites?
+    // const redirects = domains.map(([domain, repository]) => {
+    //   const [organization, repo] = repository.split('/');
+
+    //   return {
+    //     source: `/${organization}/${repo}~ref/:path*`,
+    //     has: [
+    //       {
+    //         type: 'host',
+    //         value: 'docs.page',
+    //       },
+    //     ],
+    //     destination: `https://${domain}/~ref/:path*`,
+    //   };
+    // });
+
     return [
       {
         source: '/robots.txt',
         destination: '/res/robots.txt',
         permanent: true,
       },
+      // ...redirects,
     ];
   },
   async rewrites() {
-    const domains = fs
-      .readFileSync(path.join(process.cwd(), 'domains.txt'), 'utf-8')
-      .split('\n')
-      .map(line => line.split(' '))
-      .filter(line => line.length === 2);
-
     return {
       beforeFiles: domains.map(([domain, repository]) => {
         const [organization, repo] = repository.split('/');
