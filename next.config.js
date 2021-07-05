45e69a3a7aa706ff736e5966cd389e2fb8c4b3f2
ehesp <elliot.hesp@gmail.com>
2021-07-05 14:55:53 +0100

    chore: enable redirects

--- next.config.js
+++ next.config.js
@@ -8,32 +8,32 @@ const withTM = require('next-transpile-modules')([
 const domains = require('./domains.json');
 
 // https://vercel.com/docs/environment-variables#system-environment-variables
-// const env = process.env.VERCEL_ENV;
+const env = process.env.VERCEL_ENV;
 
-// const host =
-//   env === 'production' ? 'docs.page' : env === 'preview' ? process.env.VERCEL_URL : 'localhost';
+const host =
+  env === 'production' ? 'docs.page' : env === 'preview' ? process.env.VERCEL_URL : 'localhost';
 
 module.exports = withTM({
   async redirects() {
-    // const redirects = domains.map(([domain, repository]) => {
-    //   const [organization, repo] = repository.split('/');
+    const redirects = domains.map(([domain, repository]) => {
+      const [organization, repo] = repository.split('/');
 
-    //   return {
-    //     // TODO handle refs? -> `/${organization}/${repo}~:ref/:path*`,
-    //     source: `/${organization}/${repo}/:path*`,
-    //     has: [
-    //       {
-    //         type: 'host',
-    //         value: host,
-    //       },
-    //     ],
-    //     destination:
-    //       process.env.NODE_ENV === 'production'
-    //         ? `https://${domain}/:path*`
-    //         : `http://${domain}:${process.env.PORT || 3000}/:path*`,
-    //     permanent: true,
-    //   };
-    // });
+      return {
+        // TODO handle refs? -> `/${organization}/${repo}~:ref/:path*`,
+        source: `/${organization}/${repo}/:path*`,
+        has: [
+          {
+            type: 'host',
+            value: host,
+          },
+        ],
+        destination:
+          process.env.NODE_ENV === 'production'
+            ? `https://${domain}/:path*`
+            : `http://${domain}:${process.env.PORT || 3000}/:path*`,
+        permanent: true,
+      };
+    });
 
     return [
       {
@@ -41,7 +41,7 @@ module.exports = withTM({
         destination: '/res/robots.txt',
         permanent: true,
       },
-      // ...redirects,
+      ...redirects,
     ];
   },
   async rewrites() {
@@ -60,10 +60,11 @@ module.exports = withTM({
     });
 
     const afterFiles = [];
-
     domains.forEach(([domain, repository]) => {
       const [organization, repo] = repository.split('/');
 
+      // beforeFiles sends the request here to afterFiles, so we need to
+      // make sure we don't double prefix the org/repo to the request.
       afterFiles.push({
         source: `/${organization}/${repo}`,
         has: [
