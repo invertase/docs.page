f6f03a90ba271b426f891a8c728cb73d06f1ee07
ehesp <elliot.hesp@gmail.com>
2021-07-06 11:31:22 +0100

    fix: fix incorrect error page links

--- src/templates/error/ErrorBoundary.tsx
+++ src/templates/error/ErrorBoundary.tsx
@@ -17,13 +17,11 @@ export class ErrorBoundary extends React.Component<IErrorBoundaryProps, IErrorBo
   }
 
   static getDerivedStateFromError(error: Error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return <h1>Something went wrong.</h1>;
     }
 
