0d9e96e901dada58be5ffe1df14601a6c1d3d956
Jacob Cable <jacobcable94@gmail.com>
2021-08-10 11:15:55 +0100

    feat: print undeclared variables as strings

---
+++ src/mdx/plugins/remark-undeclared-variables.ts
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import visit from 'unist-util-visit';
+import { Node } from 'unist';
+const defaultOptions = {
+  callback: () => {
+    return;
+  },
+};
+
+/**
+ * Provides a list of declared variables
+ * @returns
+ */
+
+interface DeclaredNode extends Node {
+  value: string;
+}
+interface UnDeclaredNode extends Node {
+  value: string;
+  data: any;
+}
+
+export default function rehastUndeclaredVariables(options = defaultOptions): (ast: Node) => void {
+  const keywords = ['var', 'let', 'const', 'function'];
+  const withExport = keywords.map(k => new RegExp(`(export)[ \t]+${k}[ \t]`));
+
+  const declared = [];
+
+  function visitorForDeclared(node: DeclaredNode) {
+    // Get the kind of export. This is actually stored in the Node, but the following was quicker for typescript:
+    const exportKeyword = withExport.filter(re => re.test(node.value))[0];
+
+    if (!!exportKeyword) {
+      declared.push(
+        node.value
+          .replace(exportKeyword, '')
+          .replace(/^[a-z0-9-_A-Z]*[ \t][a-z0-9-_A-Z]*[ \t]/, '')
+          .split(' ')[0],
+      );
+    }
+  }
+
+  const undeclared = [];
+
+  function visitorForUndeclared(node: UnDeclaredNode) {
+    if (!declared.includes(node.value)) {
+      undeclared.push(node.value);
+      node.type = 'text';
+      node.data = undefined;
+      node.value = `\{${node.value}\}`;
+    }
+  }
+
+  return async (ast: Node): Promise<void> => {
+    visit(ast, 'mdxjsEsm', visitorForDeclared);
+    visit(ast, 'mdxFlowExpression', visitorForUndeclared);
+    options.callback(undeclared);
+  };
+}
