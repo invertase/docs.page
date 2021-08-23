301a8a21a9ec3df6877262bba67a6974f58d2aea
Jacob Cable <jacobcable94@gmail.com>
2021-08-23 12:17:59 +0100

    feat: try api route method

--- src/pages/api/_debug/[owner]/[name]/[[...slug]].ts
+++ src/pages/api/_debug/[owner]/[name]/[[...slug]].ts
@@ -1,38 +1,70 @@
-import { getPageContent } from "../../../../../utils/content";
-import { checkExistence } from "../../../../../utils/debug";
-import { mdxSerialize } from "../../../../../utils/mdx-serialize";
-import { Properties } from "../../../../../utils/properties";
+import { getPageContent } from '../../../../../utils/content';
+import { checkExistence } from '../../../../../utils/debug';
+import { mdxSerialize } from '../../../../../utils/mdx-serialize';
+import { Properties } from '../../../../../utils/properties';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { IWarning } from '../../../../../utils/warning';
 
-export default async function handler(req, res) {
-  const {owner,name,slug} = req.query
-  const path = slug ?? [''];
-  const properties = await Properties.build([owner, name, slug ? path : '']);
+type Data = {
+  existence: { owner: string; name: string; path: string };
+  statusCode: number;
+  warnings?: IWarning;
+  errors: {
+    line?: number;
+    column?: number;
+    message?: string;
+    start?: number;
+    end?: number;
+    src?: string;
+  }[];
+};
+const defaultRes = {
+  existence: { owner: 'yes', name: 'yes', path: 'yes' },
+  statusCode: 199,
+  errors: null,
+  warnings: null,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+): Promise<void> {
+  let { owner, name, slug } = req.query;
+  owner = owner.toString();
+  name = name.toString();
+  if (!Array.isArray(slug)) {
+    slug = [slug];
+  }
+
+  const properties = await Properties.build([owner, name, ...slug]);
 
   let existence;
   try {
     existence = await checkExistence(owner, name);
-  }
-  finally {
-  if (!existence.owner || !existence.name || !existence.path) {
-    res.status(200).josn({statusCode: 404, existence})
-  }
+  } finally {
+    if (!existence.owner || !existence.name || !existence.path) {
+      res.status(200).json(defaultRes);
+    }
     // Query GitHub for the content
     const content = await getPageContent(properties);
 
     if (content.frontmatter.redirect) {
       // Redirect the user to another page
-      res.status(200).json({message: "redirect"})
+      res.status(200).json(defaultRes);
     } else {
-      let warnings,errors,serialization
+      let warnings, errors, serialization;
       try {
         serialization = await mdxSerialize(content);
-      }
-      finally {
+      } finally {
         warnings = serialization.warnings;
         errors = serialization.errors;
       }
-      res.status(200).json({warnings,errors,statusCode:500})
-  }
-  res.status(200).json({message: "all good"})
+      res.status(200).json({
+        existence: { owner: 'yes', name: 'yes', path: 'yes' },
+        statusCode: 199,
+        errors,
+        warnings,
+      });
+    }
   }
 }
