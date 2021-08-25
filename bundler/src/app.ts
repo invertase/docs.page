17c7310243a0e1f05e9c2437b94d49c10e54b4de
Jacob Cable <jacobcable94@gmail.com>
2021-08-25 13:39:49 +0100

    feat: add bundler

--- bundler/src/app.ts
+++ bundler/src/app.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response, text} from 'express';
-
+import {_debug} from './utils/debug.js'
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -10,3 +10,8 @@ app.get('/', (req, res) => res.send('Express + TypeScript Server With ESM'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is totally running at http://localhost:${PORT}`);
 });
+
+app.post('/bundle', async (req: Request, res: Response) => {
+  const bundled = await _debug(req.body.trim())
+  res.send(bundled)
+})
---
+++ bundler/src/utils/types.ts
@@ -0,0 +1,18 @@
+
+export interface Warning {
+    warningType?: "undefined component" | "undeclared variable"
+    line?: number;
+    column?: number;
+    detail?: string
+}
+export interface Error {
+    line: number;
+    column: number;
+    message: number
+}
+
+export interface Heading {
+    id: string,
+    title: string,
+    rank: number
+}
