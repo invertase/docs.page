761e331f1b556f073b4327496942e2f3dac11c90
Jacob Cable <jacobcable94@gmail.com>
2021-08-25 16:36:39 +0100

    feat: handle headingDepth on bundle endpoint

--- bundler/src/app.ts
+++ bundler/src/app.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response, text } from 'express';
-
+import express, { Request, Response, text, json} from 'express';
 import { _debug } from './utils/debug.js';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(text());
+app.use(json());
 
 app.get('/', (req, res) => res.send('mdx bundler express app.'));
 
@@ -12,7 +12,21 @@ app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
 
-app.post('/bundle', async (req: Request, res: Response) => {
+app.post('/debug', async (req: Request, res: Response) => {
   const bundled = await _debug(req?.body?.trim());
+  res.send(bundled);
+});
+
+interface BundleRequest extends Request {
+  body: string
+  query: {
+    headingDepth: string
+  }
+}
+
+app.post('/bundle', async (req: BundleRequest, res: Response) => {
+  const headingDepth = parseInt(req.query.headingDepth) || 2
+
+  const bundled = await _debug(req?.body.trim(),headingDepth);
   res.send(bundled);
 });
