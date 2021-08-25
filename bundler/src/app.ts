6a7137c69697838f7c87cf26e03fdcac8f00d81e
Jacob Cable <jacobcable94@gmail.com>
2021-08-25 15:46:05 +0100

    fix: remove rate limiter

--- bundler/src/app.ts
+++ bundler/src/app.ts
@@ -1,18 +1,10 @@
 import express, {Request, Response, text} from 'express';
-import rateLimit from "express-rate-limit";
 
 import {_debug} from './utils/debug.js'
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(text());
-
-const apiLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 minutes
-  max: 1000
-});
-
-app.use('/bundle', apiLimiter)
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server With ESM'));
 
