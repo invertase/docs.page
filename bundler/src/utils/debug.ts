ce6854a1606082c825e878019388c48d08e06ea9
Jacob Cable <jacobcable94@gmail.com>
2021-08-26 10:56:28 +0100

    feat: added incremental debug

--- bundler/src/utils/debug.ts
+++ bundler/src/utils/debug.ts
@@ -1,143 +1,34 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { bundleMDX } from 'mdx-bundler';
-import rehypeHeadings from './plugins/rehype-headings.js';
-import remarkComponentCheck from './plugins/remark-component-check.js';
-import remarkUndeclaredVariables from './plugins/remark-undeclared-variables.js';
-import remarkGfm from 'remark-gfm';
-import remarkUnwrapImages from 'remark-unwrap-images';
-import rehypeSlug from 'rehype-slug';
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
-import path from 'path';
 
-interface Warning {
-  line: number;
-  column: number;
-  message: number;
-}
-interface Error {
-  line: number;
-  column: number;
-  message: number;
+import { bundle } from "./bundle.js";
+import { setupXdmOptions } from "./xdm-options.js";
+
+export function insertCodeBlock(mdx: string,start : number,end : number) {
+  const lines : string[] = mdx.split('\n')
+  lines.splice(Math.max(start-1,0),0,'\`\`\`')
+  lines.splice(Math.min(end+1,lines.length),0,'\`\`\`')
+  return lines.join('\n')
 }
 
-interface Heading {
-  id: string;
-  title: string;
-  rank: number;
-}
 
-if (process.platform === 'win32') {
-  process.env.ESBUILD_BINARY_PATH = path.join(
-    process.cwd(),
-    'node_modules',
-    'esbuild',
-    'esbuild.exe',
-  );
-} else {
-  process.env.ESBUILD_BINARY_PATH = path.join(
-    process.cwd(),
-    'node_modules',
-    'esbuild',
-    'bin',
-    'esbuild',
-  );
-}
+export async function incrementalDebug(mdx: string, line : number) {
+  console.log('LINE',line);
+  const xdmOptionsSetup = setupXdmOptions;
 
-export const _debug: (
-  mdx: string,
-  headingDepth?: number
-) => Promise<{ warnings: Warning[]; errors: Error[]; headings: Heading[]; code: string }> = async (
-  mdx,
-  headingDepth
-) => {
-  const response = {
-    warnings: [],
-    errors: [],
-    headings: [],
-    code: '',
-  };
-  const remarkPlugins = [
-    // Checks for undefined components, converts them to text:
-    [
-      remarkComponentCheck,
-      {
-        callback: response.warnings.push.bind(response.warnings),
-      },
-    ],
-    // Checks for undeclared variables, converts them to text:
-    [
-      remarkUndeclaredVariables,
-      {
-        callback: response.warnings.push.bind(response.warnings),
-      },
-    ],
-    // Support GitHub flavoured markdown
-    remarkGfm,
-    // Ensure any `img` tags are not wrapped in `p` tags
-    remarkUnwrapImages,
-  ];
-
-  const rehypePlugins = [
-    // rehypeCodeBlocks,
-    // Add an `id` to all heading tags
-    rehypeSlug,
-    [
-      rehypeHeadings,
-      {
-        headings: headerDepthToHeaderList(headingDepth || 2),
-        callback: (headings: any) => {
-          response.headings = headings;
-        },
-      },
-    ],
-    // Make emojis accessible
-    rehypeAccessibleEmojis,
-  ];
-
-  try {
-    const bundled = await bundleMDX(mdx, {
-      xdmOptions(options) {
-        // @ts-ignore TODO fix types
-        options.remarkPlugins = [...(options.remarkPlugins ?? []), ...remarkPlugins];
-        // @ts-ignore TODO fix types
-        options.rehypePlugins = [...(options.rehypePlugins ?? []), ...rehypePlugins];
-
-        return options;
-      },
-    });
-    response.code = bundled.code;
-  } catch (e) {
-    response.errors = e.errors;
-    const lines = mdx.split('\n');
-    const length = lines.length;
-    const midpoint =Math.round(length/2);
-    const newLines = lines.slice(0,midpoint);
-    const newMdx = newLines.join('\n ');
-    try {
-        const bundled = await bundleMDX(newMdx, {
-            xdmOptions(options) {
-                // @ts-ignore TODO fix types
-                options.remarkPlugins = [...(options.remarkPlugins ?? []), ...remarkPlugins];
-                // @ts-ignore TODO fix types
-                options.rehypePlugins = [...(options.rehypePlugins ?? []), ...rehypePlugins];
+  const lines = mdx.split('\n')
+  let start = lines.length
+  
+  while (start > 0) {
+    const isolated = insertCodeBlock(mdx,start,lines.length)
+    console.log(isolated.split('\n'),start,lines.length);
     
-                return options;
-            },
-            });
-            response.code = bundled.code;
+    const attempt = await bundle(isolated,xdmOptionsSetup);
+    if (attempt.status === 200) {
+      console.log('start',start);
+      console.log(attempt?.bundled?.code);
+      
+      return {bundled: attempt, line: start -1}
     }
-    catch (e) {}
+    start = start -1;
   }
-  return response;
-};
-
-export function headerDepthToHeaderList(depth: number): string[] {
-  const list: string[] = [];
-  if (depth === 0) return list;
-
-  for (let i = 2; i <= depth; i++) {
-    list.push(`h${i}`);
-  }
-
-  return list;
 }
