322cfafa5b640cb521805f82ce14b138caf41138
Jacob Cable <jacobcable94@gmail.com>
2021-11-18 09:51:03 +0000

    chore: rename props

--- src/mdx/Table.tsx
+++ src/mdx/Table.tsx
@@ -1,7 +1,9 @@
-interface ImageProps
-  extends React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableElement>, HTMLTableElement> {}
+type TableProps = React.DetailedHTMLProps<
+  React.TableHTMLAttributes<HTMLTableElement>,
+  HTMLTableElement
+>;
 
-export function Table({ ...props }: ImageProps): JSX.Element {
+export function Table({ ...props }: TableProps): JSX.Element {
   const wrapper = (child: React.ReactElement) => <div className="overflow-scroll">{child}</div>;
 
   return wrapper(<table {...props} />);
