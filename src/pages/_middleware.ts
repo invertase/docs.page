9818d988cfd0798531c9baaaad5ea496807d9205
Jacob Cable <jacobcable94@gmail.com>
2021-11-25 09:50:32 +0000

    chore: tidied logic

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,27 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
 import domains from '../../newDomains.json';
 
-const domainsObjects = domains.map(([hostname, pathname]) => ({
-  hostname,
-  pathname: `/${pathname}`,
-}));
-
-const pathnames = domains.map(d => `/${d[1]}`);
+const paths = domains.map(d => d[1]);
 
 export default function middleware(req: NextRequest): NextResponse | void {
   const { pathname } = req.nextUrl;
+  const path = pathname.slice(1); // remove '/' at start
+  const [matchedDomain, matchedPath] = domains.find(([domain, path]) => `/${path}` === pathname);
 
   if (
-    pathnames.includes(req.nextUrl.pathname) &&
+    matchedDomain &&
+    matchedPath &&
     !pathname.includes('.') && // exclude all files in the public folder
     !pathname.endsWith('/api') // exclude all API routes
   ) {
-    // console.log(domainsObjects);
-    const { hostname } = domainsObjects.find(d => d.pathname === pathname);
     const href =
       process.env.NODE_ENV === 'production'
-        ? `https://${hostname}${pathname}`
-        : `http://localhost:3000${pathname}`;
+        ? `https://${matchedDomain}${matchedPath}`
+        : `http://localhost:3000${matchedPath}`;
 
     return NextResponse.rewrite(href);
   }
