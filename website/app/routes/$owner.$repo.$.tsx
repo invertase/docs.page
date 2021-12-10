44e2fba7c1a31d1b61ac6968fe06b6e74d9c3fc9
Jacob Cable <jacobcable94@gmail.com>
2021-12-10 14:51:19 +0000

    feat: connect up to new bundler

--- website/app/routes/$owner.$repo.$.tsx
+++ website/app/routes/$owner.$repo.$.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { LoaderFunction, MetaFunction, json, useLoaderData } from 'remix';
 import { Footer } from '~/components/Footer';
 import { Header } from '~/components/Header';
@@ -7,8 +8,17 @@ export const loader: LoaderFunction = async ({ params }) => {
   const owner = params.owner!;
   const repo = params.repo!;
   const path = params['*']!;
+  let res;
+  try {
+    res = await axios.post(
+      `http://localhost:8000/github?owner=${owner}&repository=${repo}${path ? `path=${path}` : ''}`,
+    );
+  } catch (error) {
+    //@ts-ignore
+    return error.response.data;
+  }
 
-  return json({});
+  return res.data;
 };
 
 export const meta: MetaFunction = () => ({
@@ -18,6 +28,7 @@ export const meta: MetaFunction = () => ({
 
 export default function Page() {
   const data = useLoaderData();
+  console.log(data);
 
   return (
     <>
@@ -47,7 +58,10 @@ export default function Page() {
         </div>
         <div className="pt-10 pl-72">
           <div className="mr-52 pr-16">
-            <main>Content Here</main>
+            <main>
+              Content Here
+              <div>{JSON.stringify(data.bundle)}</div>
+            </main>
             <Footer />
           </div>
           <aside className="pt-10 px-8 fixed top-14 bottom-0 w-52 overflow-y-auto right-[max(0px,calc(50%-40rem))]">
