2faac36443c73a3dfd072a3bc26abe3e3564d369
ehesp <elliot.hesp@gmail.com>
2021-12-10 14:01:28 +0000

    chore: ignore build files

--- website/public/build/routes/$owner.$repo.$-VF7QOCSN.js
+++
@@ -1,7 +0,0 @@
-import {
-  init_react
-} from "/build/_shared/chunk-6LNJWX5T.js";
-
-// browser-route-module:/Users/elliothesp/Documents/Code/oss-docs/website/app/routes/$owner.$repo.$.tsx?browser
-init_react();
-//# sourceMappingURL=/build/routes/$owner.$repo.$-VF7QOCSN.js.map
