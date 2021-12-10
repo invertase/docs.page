143a97a9950aaf2cb0f5005831f3c1db4170fcad
ehesp <elliot.hesp@gmail.com>
2021-12-10 17:05:02 +0000

    fix: update sidebar dark mode

--- website/app/components/Sidebar.tsx
+++ website/app/components/Sidebar.tsx
@@ -43,7 +43,7 @@ function removeTrailingSlash(path: string) {
 export function Sidebar() {
   return (
     <nav>
-      <ul className="text-sm text-gray-600">
+      <ul className="text-sm text-gray-600 dark:text-gray-300">
         {sidebar.map(([title, urlOrChildren]) => {
           if (typeof urlOrChildren === 'string') {
             return (
@@ -53,7 +53,7 @@ export function Sidebar() {
                   to={removeTrailingSlash(`/ehesp/testing${urlOrChildren}`)}
                   className={({ isActive }) =>
                     cx('block my-2', {
-                      'hover:text-gray-800': !isActive,
+                      'hover:text-gray-800 dark:hover:text-gray-100': !isActive,
                       'text-docs-theme font-medium border-docs-theme': isActive,
                     })
                   }
@@ -66,8 +66,8 @@ export function Sidebar() {
 
           return (
             <li className="mt-4 first:mt-0 mb-4">
-              <h5 className="text-gray-900 font-semibold tracking-wide pb-3">{title}</h5>
-              <ul className="border-l border-gray-100 dark:border-gray-800 space-y-2">
+              <h5 className="text-gray-900 dark:text-gray-200 font-semibold tracking-wide pb-3">{title}</h5>
+              <ul className="border-l border-gray-100 dark:border-gray-700 space-y-2">
                 {urlOrChildren.map(([title, url]) => (
                   <li key={url}>
                     <NavLink
@@ -75,8 +75,8 @@ export function Sidebar() {
                       to={removeTrailingSlash(`/ehesp/testing${url}`)}
                       className={({ isActive }) =>
                         cx('pl-4 -ml-px border-l border-transparent', {
-                          'hover:border-gray-400 hover:text-gray-800': !isActive,
-                          'text-docs-theme font-medium border-docs-theme': isActive,
+                          'hover:border-gray-400 hover:text-gray-800 dark:hover:text-gray-100': !isActive,
+                          'text-docs-theme font-medium !border-docs-theme': isActive,
                         })
                       }
                     >
