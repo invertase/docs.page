2116d893d96f9fe9f00fc02594adb1f6cf830a0f
Jacob Cable <jacobcable94@gmail.com>
2021-12-15 14:48:06 +0000

    feat: add basic tests

--- website/tests/example.spec.ts
+++ website/tests/example.spec.ts
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('500', async ({ page }) => {
+  await page.goto('http://localhost:3001/_test/500');
+  await page.waitForSelector('text=Error');
+});
+
+test('200', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/200');
-  
+  await page.waitForSelector('text=Hello World');
 });
