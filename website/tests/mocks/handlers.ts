d97b9274eff2921fbd60a1829e2e70a9e1365026
Jacob Cable <jacobcable94@gmail.com>
2021-12-15 15:31:59 +0000

    feat: add more screenshots

--- website/tests/mocks/handlers.ts
+++ website/tests/mocks/handlers.ts
@@ -6,12 +6,14 @@ export const handlers = [
     // const owner = req.params.owner as string;
     // const repository = req.params.repository as '200' | '400' | '404' | '500';
     const owner = req.url.searchParams.get('owner');
-    const repository = req.url.searchParams.get('repository') as unknown as
-      | '200'
-      | '400'
-      | '404'
-      | '500';
+    if (owner === '_test') {
+      const repository = req.url.searchParams.get('repository') as unknown as
+        | '200'
+        | '400'
+        | '404'
+        | '500';
 
-    return res(ctx.status(parseInt(repository)), ctx.json(fixtures[repository]));
+      return res(ctx.status(parseInt(repository)), ctx.json(fixtures[repository]));
+    }
   }),
 ];
--- website/tests/screenshots.spec.ts
+++ website/tests/screenshots.spec.ts
@@ -8,3 +8,12 @@ test('200', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/200');
   await takeScreenshot(page, '200');
 });
+
+
+const projects = ['invertase/melos', 'csells/go_router'];
+projects.forEach(project => {
+  test(project, async ({ page }) => {
+    await page.goto(`http://localhost:3001/${project}`);
+    await takeScreenshot(page, project);
+  });
+});
