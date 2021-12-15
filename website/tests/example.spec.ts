20c82a872fb523d68e591fe02f745b5640559c05
Jacob Cable <jacobcable94@gmail.com>
2021-12-15 14:53:38 +0000

    feat: two more tests

--- website/tests/example.spec.ts
+++ website/tests/example.spec.ts
@@ -9,3 +9,13 @@ test('200', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/200');
   await page.waitForSelector('text=Hello World');
 });
+
+test('400', async ({ page }) => {
+  await page.goto('http://localhost:3001/_test/400');
+  await page.waitForSelector('text=Bad Request');
+});
+
+test('404', async ({ page }) => {
+  await page.goto('http://localhost:3001/_test/404');
+  await page.waitForSelector('text=Page not found');
+});
