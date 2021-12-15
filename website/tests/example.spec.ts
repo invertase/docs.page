e890db579e1e2ecec8b6d01be8abda710bd03a86
Jacob Cable <jacobcable94@gmail.com>
2021-12-15 15:09:34 +0000

    fix: fix tests

--- website/tests/example.spec.ts
+++ website/tests/example.spec.ts
@@ -1,21 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+
+async function assertExists(page: Page, selector: string) {
+  await page.waitForSelector(selector, { timeout: 10000 });
+}
 
 test('500', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/500');
-  await page.waitForSelector('text=Error');
+  await assertExists(page, 'text=Error');
 });
 
 test('200', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/200');
-  await page.waitForSelector('text=Hello World');
+  await assertExists(page, 'text=Hello World');
 });
 
 test('400', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/400');
-  await page.waitForSelector('text=Bad Request');
+  await assertExists(page, 'text=Bad Request');
 });
 
 test('404', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/404');
-  await page.waitForSelector('text=Page not found');
+  await assertExists(page, 'text=Page not found');
 });
