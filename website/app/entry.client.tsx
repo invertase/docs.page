c4771de4a92811c64a739de0495ce1ecfe2ed6eb
Jacob Cable <jacobcable94@gmail.com>
2021-12-15 10:59:37 +0000

    chore: add scripts for testing

--- website/app/entry.client.tsx
+++ website/app/entry.client.tsx
@@ -1,4 +1,4 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
-// require('../tests/mocks');
+if (process.env.MSW_ENABLED === '1') { require('../tests/mocks') };
 hydrate(<RemixBrowser />, document);
