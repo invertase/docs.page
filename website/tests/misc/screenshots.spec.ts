e622e0af375d8938330df6b011de941f5ecae365
Jacob Cable <jacobcable94@gmail.com>
2021-12-16 16:54:42 +0000

    chore: reorganized tests

--- website/tests/misc/screenshots.spec.ts
+++ website/tests/misc/screenshots.spec.ts
@@ -4,15 +4,14 @@ const takeScreenshot = async (page: Page, fileName: string) => {
   await page.screenshot({ path: `tests/screenshots/${fileName}.png` });
 };
 
-test('200', async ({ page }) => {
+test.skip('200', async ({ page }) => {
   await page.goto('http://localhost:3001/_test/200');
   await takeScreenshot(page, '200');
 });
 
-
 const projects = ['invertase/melos', 'csells/go_router'];
 projects.forEach(project => {
-  test(project, async ({ page }) => {
+  test.skip(project, async ({ page }) => {
     await page.goto(`http://localhost:3001/${project}`);
     await takeScreenshot(page, project);
   });
