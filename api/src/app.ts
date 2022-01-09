ab56a57e9409aad6bc1cb11197ffdbff6a14f306
Jacob Cable <jacobcable94@gmail.com>
2022-01-09 10:54:04 +0000

    fix: rebuild api

--- api/src/app.ts
+++ api/src/app.ts
@@ -25,9 +25,9 @@ app.use(
     extended: true,
   })
 );
+app.get("/", (req, res) => res.send("Express + TypeScript Server With ESM"));
 app.use("/", routes);
 // API Routes
-app.get("/", (req, res) => res.send("Express + TypeScript Server With ESM"));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is totally running at http://localhost:${PORT}`);
