a2443e335cff72c4d46b034ef776bea2bb98fe12
Jacob Cable <jacobcable94@gmail.com>
2022-03-07 13:55:09 +0000

    fix: fix links again

--- website/app/components/DocsLink.tsx
+++ website/app/components/DocsLink.tsx
@@ -48,7 +48,7 @@ export function DocsLink({ ...props }: NavLinkProps): JSX.Element {
     );
   }
 
-  if (domain && process.env.NODE_ENV === 'production') {
+  if (domain) {
 
     return (
       <a
