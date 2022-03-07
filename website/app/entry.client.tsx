1e3875f843d2023ef8da6a034e668900fe165278
Jacob Cable <jacobcable94@gmail.com>
2022-03-07 16:38:28 +0000

    fix: add conditional mutations on window to clinet entry

--- website/app/entry.client.tsx
+++ website/app/entry.client.tsx
@@ -4,4 +4,21 @@ import { RemixBrowser } from 'remix';
 if (window.ENV.MSW_ENABLED === '1') {
   require('../tests/mocks');
 }
+
+if (
+  process.env.NODE_ENV === "production" &&
+  window.location.host !== "docs.page"
+) {
+  window.__remixManifest.routes["routes/$owner.$repo.$"].path = "*";
+  window.__remixManifest.routes["routes/$owner.$repo.$"].index = false;
+  delete window.__remixManifest.routes["routes/index"];
+  delete window.__remixManifest.routes["routes/preview"];
+  delete window.__remixManifest.routes["routes/preview-fetch"];
+  window.__remixManifest.routes["routes/$owner.$repo.$2"] = Object.assign(
+    {},
+    window.__remixManifest.routes["routes/$owner.$repo.$"]
+  );
+  window.__remixManifest.routes["routes/$owner.$repo.$2"].path = "/";
+}
+
 hydrate(<RemixBrowser />, document);
