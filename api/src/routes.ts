9f33d9e3e528e213fae245c0591d2bdf94cdc95a
Jacob Cable <jacobcable94@gmail.com>
2022-03-11 12:31:05 +0000

    feat: add ref handling to custom-css resource path

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import * as rawController from './controllers/raw.js';
 import * as githubController from './controllers/github.js';
 import * as statusController from './controllers/status.js';
+import * as configController from './controllers/config.js';
+
 // import ProbotMiddleWare from './controllers/bot.js';
 
 const router = Router();
 
 router.get('/status', statusController.statusRes);
 router.get('/bundle', githubController.bundleGitHub);
+router.get('/config', configController.getConfig);
 router.post('/raw', rawController.bundleRaw);
+
 // router.post('/webhooks/bot-docs-page', (req, res) => ProbotMiddleWare(req, res))
 export default router;
--- website/app/routes/$owner.$repo.custom-styles[.]css.ts
+++ website/app/routes/$owner.$repo.custom-styles[.]css.ts
@@ -1,18 +1,28 @@
 import type { LoaderFunction } from 'remix';
 
-async function getConfig({ owner, repo }: Record<string, string>) {
-  const endpoint = `https://raw.githubusercontent.com/${owner}/${repo}/main/docs.json`;
-  const res = await (await fetch(endpoint)).text();
+async function getConfiguration({ owner, repo, ref }: Record<string, string>) {
 
-  return JSON.parse(res);
+  const host = process.env.NODE_ENV === 'production' ? 'https://api.docs.page' : 'http://localhost:8000';
+
+  const endpoint = `${host}/config?owner=${owner}&repository=${repo}&ref=${ref}`;
+
+  const { config } = await (await fetch(endpoint)).json();
+
+  return config;
 }
 
-export const loader: LoaderFunction = async ({ request, params }) => {
-  const config = await getConfig({ owner: params.owner!, repo: params.repo! });
+export let loader: LoaderFunction = async ({ params }) => {
+
+  const owner = params.owner!
+
+  const [repo, ref] = params.repo!.split('~')
+
+  const config = await getConfiguration({ owner: params.owner!, repo, ref });
 
   let css: Response = new Response();
 
   if (config.experimentalMath) {
+    console.log('here')
     const response = await fetch('https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/katex.min.css', {
       integrity: 'sha384-RZU/ijkSsFbcmivfdRBQDtwuwVqK7GMOw6IMvKyeWL2K5UAlyp6WonmB8m7Jd0Hn',
     });
