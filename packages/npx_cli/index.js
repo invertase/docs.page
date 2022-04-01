7f66d779aa4c5e528a68915b4b7c7c6a4b5b1743
Jacob Cable <jacobcable94@gmail.com>
2022-04-01 12:50:03 +0100

    chore: update npm package

---
+++ packages/npx_cli/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+var path = require('path');
+var fs = require('fs');
+var childProcess = require('child_process');
+
+var binary = path.join(__dirname, 'binaries', process.platform, `docs_page${process.platform === 'win32' ? '.exe' : ''}`);
+
+if (!fs.existsSync(binary)) {
+    throw new Error('Docs.page does not support this platform')
+};
+
+var docsPage = childProcess.spawn(binary, process.argv.slice(2), { stdio: ['inherit', 'inherit', 'inherit'], env: process.env });
+
+docsPage.on('exit', (event) => process.exit(event.code));
+
+
+process.on('SIGTERM', (event) => docsPage.kill('SIGTERM'));
+
+process.on('SIGINT', (event) => docsPage.kill('SIGINT'));
+
+
+
+
+
