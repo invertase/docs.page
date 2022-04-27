806881219d3af32f656d0d3831b926cbbf24f4eb
Jacob Cable <jacobcable94@gmail.com>
2022-04-27 12:21:24 +0100

    fix: correct vimeo aspect ratio issues

--- website/app/components/mdx/Vimeo.tsx
+++ website/app/components/mdx/Vimeo.tsx
@@ -1,13 +1,23 @@
 import Embed, { VimeoProps as ReactVimeoProps } from '@u-wave/react-vimeo';
-
-const DEFAULT_WIDTH = 400;
+import { useState } from 'react';
 
 type VimeoProps = ReactVimeoProps & { caption?: string };
 
 function Vimeo(props: VimeoProps) {
+  const [ready, setReady] = useState<boolean>(false);
+
   return (
-    <figure className="flex aspect-video w-full items-center justify-center overflow-hidden rounded">
-      <Embed {...{ width: DEFAULT_WIDTH, ...props }} />
+    <figure
+      id="vimeo-wrapper"
+      className="mx-auto overflow-hidden rounded shadow"
+      style={{ width: props.width || 'inherit' }}
+    >
+      {!ready && (
+        <div className="mx-auto flex h-64 items-center justify-center rounded">
+          <span>Loading...</span>
+        </div>
+      )}
+      <Embed onReady={() => setReady(true)} responsive={true} {...props} />
       {props.caption && (
         <figcaption className="mt-4 text-center text-xs dark:text-white">
           {props.caption}
