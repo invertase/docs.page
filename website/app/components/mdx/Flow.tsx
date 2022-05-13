35b763dc03cee70837f20d67475800ad7af5ca2f
Jacob Cable <jacobcable94@gmail.com>
2022-05-13 10:32:10 +0100

    feat: add extra props to react-flow component

--- website/app/components/mdx/Flow.tsx
+++ website/app/components/mdx/Flow.tsx
@@ -8,6 +8,7 @@ import ReactFlow, {
   Node,
   Edge,
   ReactFlowInstance,
+  ReactFlowProps,
 } from 'react-flow-renderer';
 
 const onInit = (reactFlowInstance: ReactFlowInstance) =>
@@ -17,19 +18,30 @@ const Flow = ({
   initialNodes,
   initialEdges,
   height,
+  minimap,
+  controls,
+  caption,
+  reactFlowProps,
 }: {
   initialNodes: Node[];
   initialEdges: Edge[];
   height?: number;
+  minimap?: boolean;
+  controls?: boolean;
+  caption?: string;
+  reactFlowProps: ReactFlowProps;
 }) => {
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   //@ts-ignore
   const onConnect = params => setEdges(eds => addEdge(params, eds));
 
+  const containerClassName = height ? `w-full h-[${height}]` : `w-full h-[800px]`;
+
   return (
-    <div className={`w-full h-[${height || '800px'}]`}>
+    <figure className={containerClassName}>
       <ReactFlow
+        {...reactFlowProps}
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
@@ -39,28 +51,32 @@ const Flow = ({
         fitView
         attributionPosition="top-right"
       >
-        <MiniMap
-          //@ts-ignore
-          nodeStrokeColor={n => {
-            if (n.style?.background) return n.style.background;
-            if (n.type === 'input') return '#0041d0';
-            if (n.type === 'output') return '#ff0072';
-            if (n.type === 'default') return '#1a192b';
+        {minimap && (
+          <MiniMap
+            //@ts-ignore
+            nodeStrokeColor={n => {
+              if (n.style?.background) return n.style.background;
+              if (n.type === 'input') return '#0041d0';
+              if (n.type === 'output') return '#ff0072';
+              if (n.type === 'default') return '#1a192b';
 
-            return '#eee';
-          }}
-          //@ts-ignore
-          nodeColor={n => {
-            if (n.style?.background) return n.style.background;
+              return '#eee';
+            }}
+            //@ts-ignore
+            nodeColor={n => {
+              if (n.style?.background) return n.style.background;
 
-            return '#fff';
-          }}
-          nodeBorderRadius={2}
-        />
-        <Controls />
+              return '#fff';
+            }}
+            nodeBorderRadius={2}
+          />
+        )}
+
+        {controls && <Controls />}
         <Background color="#aaa" gap={16} />
       </ReactFlow>
-    </div>
+      <figcaption className="my-3 text-center text-sm italic dark:text-white">{caption}</figcaption>
+    </figure>
   );
 };
 
