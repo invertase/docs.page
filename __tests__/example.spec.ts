507160725fb3644ae568c776b6967882e0f14621
Jacob Cable <jacobcable94@gmail.com>
2022-10-28 15:07:05 +0100

    test: set initial screenshots

--- __tests__/example.spec.ts
+++ __tests__/example.spec.ts
@@ -4,7 +4,11 @@ import {default as domains} from '../domains.json';
 
 const stagingUrl = 'https://staging-docs-page-website-euw1-dzpolnxswq-ew.a.run.app';
 
+// skip these as these are not working (user error)
+const skipList  = ['dokumentacja.otwartaturystyka.pl']
+
 domains.forEach(domain => {
+    if (!skipList.includes(domain[0])) {
     test(`${domain[0]}`, async ({ page }) => {
 
         const response = await page.goto(`https://${domain[0]}`);
@@ -18,4 +22,5 @@ domains.forEach(domain => {
         await page.goto(`${stagingUrl}/${domain[1]}`)
         await expect(page).toHaveScreenshot(`${domain[1]}-original.png`);
     });
+}
 })
