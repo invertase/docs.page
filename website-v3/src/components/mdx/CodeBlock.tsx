0ee70cc4dfad271da12b156077e2e0a0651e006c
Elliot Hesp <elliot.hesp@gmail.com>
2022-12-14 15:42:11 +0000

    copy code button

--- website-v3/src/components/mdx/CodeBlock.tsx
+++ website-v3/src/components/mdx/CodeBlock.tsx
@@ -4,7 +4,18 @@ export interface CodeBlockProps extends React.HTMLProps<HTMLPreElement> {
 }
 
 const CodeBlock: React.FC<CodeBlockProps> = props => {
-  return <div dangerouslySetInnerHTML={{ __html: props.html }}></div>;
+  return (
+    <div className="group relative">
+      <button
+        data-copy
+        data-copy-value={props.raw}
+        className="absolute top-2 right-2 rounded bg-black px-3 py-1 font-mono text-xs opacity-0 transition hover:bg-black/50 group-hover:opacity-100 data-[copied=true]:[&>span]:hidden"
+      >
+        Copy
+      </button>
+      <div dangerouslySetInnerHTML={{ __html: props.html }}></div>
+    </div>
+  );
 };
 
 export default CodeBlock;
