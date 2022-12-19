389e210132456efaa571df4291e8a61bceb135df
Elliot Hesp <elliot.hesp@gmail.com>
2022-12-19 15:08:08 +0000

    previous/next

--- website-v3/src/components/icons.tsx
+++ website-v3/src/components/icons.tsx
@@ -30,6 +30,30 @@ export const SunIcon = () => (
   </svg>
 );
 
+export const ChevronRightIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+  </svg>
+);
+
+export const ChevronLeftIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+  </svg>
+);
+
 export const MoonIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
