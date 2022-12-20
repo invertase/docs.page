f900d114ce49cc838798a05df90c980839c49025
Elliot Hesp <elliot.hesp@gmail.com>
2022-12-20 16:31:45 +0000

    eyebrow

--- website-v3/src/components/mdx/Heading.tsx
+++ website-v3/src/components/mdx/Heading.tsx
@@ -1,14 +1,21 @@
 import { createElement } from 'react';
+import cx from 'classnames';
 
 interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   id?: string;
+  eyebrow?: boolean;
   type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
 const Heading: React.FC<HeadingProps> = props => {
-  const { id, type, ...other } = props;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { id, type, eyebrow, ...other } = props;
+
   const el = createElement(props.type, {
     ...other,
+    className: cx(props.className, {
+      'text-docs-theme': eyebrow,
+    }),
   });
 
   return el;
