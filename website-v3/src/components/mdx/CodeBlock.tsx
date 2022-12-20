a96803db0851a4ffcb11833296bf5c41f9c304a6
Elliot Hesp <elliot.hesp@gmail.com>
2022-12-20 12:25:29 +0000

    -

--- website-v3/src/components/mdx/CodeBlock.tsx
+++ website-v3/src/components/mdx/CodeBlock.tsx
@@ -10,9 +10,9 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
   console.log('title', props.title);
   return (
     <div>
-      <div className="group mb-6 overflow-hidden rounded-xl ring-white/10 dark:ring-1">
+      <div className="group mb-6 rounded-xl">
         {!!props.title && (
-          <div className="border-b border-white/10 bg-[#19191d] py-2 px-4">
+          <div className="rounded-t-xl border border-white/10 bg-[#19191d] py-2 px-4">
             <span className="text-xs font-semibold tracking-wide text-white">{props.title}</span>
           </div>
         )}
@@ -20,13 +20,16 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
           <button
             data-copy
             data-copy-value={props.raw}
-            className="absolute top-2 right-2 rounded bg-[#19191d] px-3 py-1 font-mono text-xs opacity-0 transition hover:bg-black/50 group-hover:opacity-100 data-[copied=true]:[&>span]:hidden"
+            className="absolute top-2 right-2 rounded bg-[#19191d] px-3 py-1 font-mono text-xs text-white opacity-0 transition hover:bg-black/50 group-hover:opacity-100 data-[copied=true]:[&>span]:hidden"
           >
             Copy
           </button>
           <div
             dangerouslySetInnerHTML={{ __html: props.html }}
-            className="overflow-hidden text-sm [&>pre>code>.line]:leading-6"
+            className={cx('overflow-hidden border-white/10 text-sm [&>pre>code>.line]:leading-6', {
+              'rounded-xl border': !props.title,
+              'rounded-b-xl dark:border-b dark:border-r dark:border-l': !!props.title,
+            })}
           />
         </div>
       </div>
