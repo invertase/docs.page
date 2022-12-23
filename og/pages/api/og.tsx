cbf92a0e5159619db326e0cffff7ce60bf6a38e2
Elliot Hesp <elliot.hesp@gmail.com>
2022-12-23 20:19:23 +0000

    -

--- og/pages/api/og.tsx
+++ og/pages/api/og.tsx
@@ -4,25 +4,10 @@ export const config = {
   runtime: 'edge',
 };
 
-export default function (req: Request) {
-  const url = new URL(req.url);
-  const params = url.searchParams;
-
-  const owner = params.get('owner');
-  const repository = params.get('repository');
-
-  if (!owner || !repository) {
-    return new Response('Missing owner or repository', {
-      status: 400,
-    });
-  }
-
-  const logo = params.get('logo');
-  const name = params.get('name');
-  const description = params.get('description');
-
+export default async function () {
   return new ImageResponse(
-    <div
+    (
+      <div
         style={{
           height: '100%',
           width: '100%',
@@ -36,15 +21,34 @@ export default function (req: Request) {
         <div tw="bg-gray-50 flex">
           <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
             <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
-              <span>{owner}/{repository}</span>
-              <span tw="text-indigo-600">{description}</span>
+              <span>Ready to dive in?</span>
+              <span tw="text-indigo-600">Start your free trial today.</span>
             </h2>
+            <div tw="mt-8 flex md:mt-0">
+              <div tw="flex rounded-md shadow">
+                <a
+                  href="#"
+                  tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white"
+                >
+                  Get started
+                </a>
+              </div>
+              <div tw="ml-3 flex rounded-md shadow">
+                <a
+                  href="#"
+                  tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>,
+      </div>
+    ),
     {
       width: 1200,
-      height: 600,
+      height: 630,
     },
   );
 }
