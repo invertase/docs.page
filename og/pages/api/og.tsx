baf9fd5ade4c1cc693a76471a67e519b45c86728
Elliot Hesp <elliot.hesp@gmail.com>
2022-12-23 20:39:59 +0000

    -

--- og/pages/api/og.tsx
+++ og/pages/api/og.tsx
@@ -39,13 +39,7 @@ export default function handler(req: NextRequest) {
               justifyItems: 'center',
             }}
           >
-            <img
-              alt="Vercel"
-              height={200}
-              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-              style={{ margin: '0 30px' }}
-              width={232}
-            />
+            Lol image broke
           </div>
           <div
             style={{
