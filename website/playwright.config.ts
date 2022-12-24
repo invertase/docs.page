4242964d01f6d8be8a12b2348d5413bdf91ebf2c
Salakar <mike.diarmid@gmail.com>
2022-12-24 02:38:12 +0000

    refactor: remove v2 website directory

--- website/playwright.config.ts
+++
@@ -1,6 +0,0 @@
-import { PlaywrightTestConfig } from '@playwright/test';
-
-const config: PlaywrightTestConfig = {
-  globalTimeout: 5 * 60 * 1000, // 5 min global timeout
-};
-export default config;
--- website/tests/routes/homepage.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-import { assertExists } from '../helpers';
-
-test('Homepage  should load', async ({ page }) => {
-  await page.goto('http://localhost:3001');
-  await assertExists(page, 'text=docs.page');
-  await assertExists(page, 'text=Instant Open Source docs with zero configuration.');
-});
