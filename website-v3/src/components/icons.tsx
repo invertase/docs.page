fee9854aabf62e72bb8d27d4cb4ed634eea9a5f1
Elliot Hesp <elliot.hesp@gmail.com>
2023-01-04 09:56:37 +0000

    feat: menu toggle

--- website-v3/src/components/icons.tsx
+++ website-v3/src/components/icons.tsx
@@ -127,3 +127,19 @@ export const CheckCircleIcon = () => (
     />
   </svg>
 );
+
+export const BarsIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
+  </svg>
+);
