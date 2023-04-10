346f9d5101decd3a75e6cc1b0d98c2be7b1a6d6f
Gregory Conrad <gregorysconrad@gmail.com>
2023-04-10 06:15:19 -0400

    feat: allow importing Zapps from other locations (#278)

--- website-v3/src/components/mdx/Zapp.tsx
+++ website-v3/src/components/mdx/Zapp.tsx
@@ -9,12 +9,19 @@ const Zapp: React.FC<ZappProps> = props => {
     return <div />;
   }
 
+  // Allows for GitHub, Gist, and other Zapps.
+  // If no / is included, we assume the user supplied a Zapp id,
+  // like 'flutter', which should be prefixed by 'edit/'.
+  // Otherwise, we assume the user specified their own Zapp path,
+  // and treat it as such.
+  const id = props.id.includes('/') ? props.id : `edit/${props.id}`;
+
   const lazy = props.lazy === undefined ? false : props.lazy;
   const theme = props.theme || 'dark';
 
   return (
     <iframe
-      src={`https://zapp.run/embed/${props.id}?theme=${theme}&lazy=${lazy}`}
+      src={`https://zapp.run/${id}?theme=${theme}&lazy=${lazy}`}
       className="aspect-video w-full"
       style={{
         width: '100%',
