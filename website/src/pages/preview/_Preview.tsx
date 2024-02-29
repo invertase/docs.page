7197eee453175fa28d07db9da26909d86e6266f2
Majid <mhadaily@gmail.com>
2024-02-29 16:36:26 +0100

    add old landing page

--- website/src/pages/preview/_Preview.tsx
+++ website/src/pages/preview/_Preview.tsx
@@ -1,3 +1,4 @@
+import { detect } from 'detect-browser';
 import { getPreview, type GetPreviewResponse } from 'src/bundle';
 import context, { type Context } from 'src/context';
 import { replaceMoustacheVariables } from 'src/utils';
@@ -5,6 +6,7 @@ import { openDB } from 'idb';
 import { useEffect } from 'react';
 import { useStore } from '@nanostores/react';
 import DocsView from '@layouts/DocsView';
+import Footer from '@layouts/Footer';
 
 interface FileEntry {
   name: string;
@@ -14,6 +16,7 @@ interface FileEntry {
 export default function Preview(props: { previewPath?: string | undefined }) {
   const { previewPath } = props;
   const { isPreviewReady } = useStore(context);
+  const browser = detect();
 
   useEffect(() => {
     const DB_NAME = 'docspage-preview-store';
@@ -315,13 +318,51 @@ export default function Preview(props: { previewPath?: string | undefined }) {
   return isPreviewReady ? (
     <DocsView />
   ) : (
-    <div id="directory-container" className="flex h-screen flex-col items-center justify-center">
-      <button
-        id="select-directory"
-        className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-      >
-        Select Directory
-      </button>
+    <div className="dark:text-white">
+      <section className="items-center py-16 px-4 text-center lg:py-32 lg:text-left">
+        <div className="mx-auto max-w-6xl tracking-wider">
+          <div className="mb-4 flex items-center justify-center space-x-4 lg:justify-between">
+            <div className="flex items-center justify-start space-x-4 text-center">
+              <h3 className="font-anton text-4xl">docs.page</h3>
+              <h3 className="italic">Local Preview Mode (Beta)</h3>
+            </div>
+          </div>
+          <h1 className=" font-anton mt-40 mb-4 bg-gradient-to-br from-gray-900 via-gray-700 to-gray-900 bg-clip-text text-center text-2xl text-transparent dark:from-gray-100 dark:via-gray-300 dark:to-gray-200 lg:text-5xl">
+            Preview from your machine, with{' '}
+            <span className="bg-gradient-to-br from-red-600 to-black bg-clip-text text-transparent dark:from-yellow-200 dark:to-red-400">
+              hot reload.
+            </span>{' '}
+            <span className="bg-gradient-to-br from-red-800 to-violet-500 bg-clip-text text-transparent"></span>
+          </h1>
+          {browser?.name === 'chrome' ? (
+            <>
+              <p suppressHydrationWarning className="pt-20 text-center">
+                To get started, simply select the local directory containing your docs.json config
+                file:
+              </p>
+              <div className="w-100% content-center items-center justify-center pt-8 text-center">
+                <button
+                  className="text-s cursor-pointer whitespace-nowrap rounded-lg bg-green-600 px-3 py-2 text-white shadow transition-colors hover:bg-green-500"
+                  id="select-directory"
+                >
+                  Select Directory!
+                </button>
+                <div
+                  id="directory-container"
+                  className="flex h-screen flex-col items-center justify-center"
+                ></div>
+              </div>
+            </>
+          ) : (
+            <p suppressHydrationWarning className="pt-20 text-center">
+              Local Preview Mode is only available on Chrome at the moment, sorry :(
+            </p>
+          )}
+        </div>
+        <div className="mx-auto mt-32 max-w-5xl px-4 lg:px-0">
+          <Footer />
+        </div>
+      </section>
     </div>
   );
 }
