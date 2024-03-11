0a067e93450a3bba7e515d187e82c0edfe96df45
Majid <mhadaily@gmail.com>
2024-03-11 08:21:20 +0100

    remove douplicated astro files

--- website/src/components/PreviousNext.tsx
+++ website/src/components/PreviousNext.tsx
@@ -1,7 +1,7 @@
 import context from 'src/context';
 import { ChevronLeftIcon, ChevronRightIcon } from './icons';
 import Link from './Link';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useStore } from '@nanostores/react';
 
 // title, href
@@ -14,40 +14,38 @@ export default function PreviousNext() {
   const [previous, setPrevious] = useState<Anchor | undefined>();
   const [next, setNext] = useState<Anchor | undefined>();
 
-  useEffect(() => {
-    const links: [string, string][] = [];
+  const links: [string, string][] = [];
 
-    // Finds any links in the sidebar and pushes them to a flattened array.
-    function iterateSidebarArray(array: typeof sidebar) {
-      for (const [title, urlOrChildren] of array) {
-        if (typeof urlOrChildren === 'string') links.push([title, urlOrChildren]);
-        else if (Array.isArray(urlOrChildren)) iterateSidebarArray(urlOrChildren);
-      }
+  // Finds any links in the sidebar and pushes them to a flattened array.
+  function iterateSidebarArray(array: typeof sidebar) {
+    for (const [title, urlOrChildren] of array) {
+      if (typeof urlOrChildren === 'string') links.push([title, urlOrChildren]);
+      else if (Array.isArray(urlOrChildren)) iterateSidebarArray(urlOrChildren);
     }
+  }
 
-    // Iterate the sidebar
-    iterateSidebarArray(sidebar);
+  // Iterate the sidebar
+  iterateSidebarArray(sidebar);
 
-    const titleForLink = (link: string) => {
-      return links.find(([_, href]) => href === link)?.at(0) || null;
-    };
+  const titleForLink = (link: string) => {
+    return links.find(([_, href]) => href === link)?.at(0) || null;
+  };
 
-    // If the user has specified a previous or next page, use that.
-    if (frontmatter.previous)
-      setPrevious([
-        frontmatter.previousTitle || titleForLink(frontmatter.previous) || '',
-        frontmatter.previous,
-      ]);
-    if (frontmatter.next)
-      setNext([frontmatter.nextTitle || titleForLink(frontmatter.next) || '', frontmatter.next]);
+  // If the user has specified a previous or next page, use that.
+  if (frontmatter.previous)
+    setPrevious([
+      frontmatter.previousTitle || titleForLink(frontmatter.previous) || '',
+      frontmatter.previous,
+    ]);
+  if (frontmatter.next)
+    setNext([frontmatter.nextTitle || titleForLink(frontmatter.next) || '', frontmatter.next]);
 
-    // If the user wants auto infered previous/next, find them (if not already set).
-    if (automaticallyInferNextPrevious) {
-      const currentIndex = links.findIndex(([_, href]) => href === relativePath);
-      if (currentIndex > -1 && !previous) setPrevious(links[currentIndex - 1]);
-      if (currentIndex > -1 && !next) setNext(links[currentIndex + 1]);
-    }
-  }, []);
+  // If the user wants auto infered previous/next, find them (if not already set).
+  if (automaticallyInferNextPrevious) {
+    const currentIndex = links.findIndex(([_, href]) => href === relativePath);
+    if (currentIndex > -1 && !previous) setPrevious(links[currentIndex - 1]);
+    if (currentIndex > -1 && !next) setNext(links[currentIndex + 1]);
+  }
 
   return !!previous || !!next ? (
     <nav className="flex items-center pt-6 text-sm font-semibold tracking-wide text-black dark:text-white">
--- website/src/layouts/Navigation.tsx
+++ website/src/layouts/Navigation.tsx
@@ -1,4 +1,5 @@
 import { useStore } from '@nanostores/react';
+import { useEffect } from 'react';
 import context from 'src/context';
 
 export default function Navigation() {
@@ -7,24 +8,27 @@ export default function Navigation() {
   const links = nav?.querySelectorAll('a[data-id]');
   const sections = document.querySelectorAll('main section[data-id]');
 
-  if (links && sections) {
-    window.onscroll = () => {
-      let spied: Element | null = null;
+  useEffect(() => {
+    if (links && sections) {
+      window.onscroll = () => {
+        let spied: Element | null = null;
 
-      for (const link of links) link.classList.remove('text-docs-theme');
+        for (const link of links) link.classList.remove('text-docs-theme');
 
-      for (const section of sections) {
-        const span = section.querySelector('span:first-child');
-        if (span && span.getBoundingClientRect().y < 1) spied = span;
-      }
+        for (const section of sections) {
+          const span = section.querySelector('span:first-child');
+          if (span && span.getBoundingClientRect().y < 1) spied = span;
+        }
 
-      if (spied) {
-        const id = spied.getAttribute('id')!;
-        const link = nav.querySelector(`a[data-id="${id}"]`);
-        link?.classList.add('text-docs-theme');
-      }
-    };
-  }
+        if (spied) {
+          const id = spied.getAttribute('id')!;
+          const link = nav.querySelector(`a[data-id="${id}"]`);
+          link?.classList.add('text-docs-theme');
+        }
+      };
+    }
+  }, []);
+
   return (
     <ul id="navigation">
       {headings.map((heading, index) => (
