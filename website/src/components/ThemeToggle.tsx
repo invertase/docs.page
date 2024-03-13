afb4f2b4b29e4185369f372f8c8639f6a5dd6ea4
Majid <mhadaily@gmail.com>
2024-03-13 19:38:16 +0100

    return support for main astro render pages and preview must be fixed for header

--- website/src/components/ThemeToggle.tsx
+++ website/src/components/ThemeToggle.tsx
@@ -1,56 +1,14 @@
-import { useStore } from '@nanostores/react';
 import { MoonIcon, SunIcon } from './icons';
-import context from 'src/context';
-import { useEffect, useState } from 'react';
 
 export default function ThemeToggle() {
-  const { owner, repository, domain, ref, theme } = useStore(context);
-  const [themeMode, setThemeMode] = useState(theme || 'dark');
-
-  const isDark = themeMode === 'dark';
-
-  useEffect(() => {
-    context.setKey('theme', themeMode);
-    document.querySelector('html')?.setAttribute('data-theme', themeMode);
-  }, [themeMode]);
-
-  const toggleThemeMode = () => {
-    if (isDark) {
-      setThemeMode('light');
-    } else {
-      setThemeMode('dark');
-    }
-
-    // Update the theme via server and return a cookie
-    fetch('/api/docs.page/theme', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        owner,
-        repository,
-        ref,
-        domain,
-        theme: themeMode,
-      }),
-    });
-  };
   return (
-    <button
-      data-theme-toggle
-      onClick={toggleThemeMode}
-      className="relative h-6 w-6 transition-opacity hover:opacity-75"
-    >
-      {themeMode === 'dark' ? (
-        <span data-theme-type="dark">
-          <MoonIcon />
-        </span>
-      ) : (
-        <span data-theme-type="light">
-          <SunIcon />
-        </span>
-      )}
+    <button data-theme-toggle className="relative h-6 w-6 transition-opacity hover:opacity-75">
+      <span data-theme-type="dark" className="hidden">
+        <MoonIcon />
+      </span>
+      <span data-theme-type="light" className="hidden">
+        <SunIcon />
+      </span>
     </button>
   );
 }
