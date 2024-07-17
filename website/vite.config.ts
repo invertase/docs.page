1909ec84e3b65d3e06879951f219028e3b11d0b4
Elliot Hesp <elliot.hesp@gmail.com>
2024-07-17 17:30:05 +0100

    use native fetch

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,19 +4,19 @@ import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-installGlobals();
+installGlobals({ nativeFetch: true });
 
 export default defineConfig({
-	plugins: [
-		remix({
-			presets: [vercelPreset()],
-			future: {
-				unstable_singleFetch: true,
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-			},
-		}),
-		tsconfigPaths(),
-	],
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+      future: {
+        unstable_singleFetch: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
