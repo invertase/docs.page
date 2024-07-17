d200d2f13a817f284037bdeed178f2497fa91bc9
Elliot Hesp <elliot.hesp@gmail.com>
2024-07-17 17:29:16 +0100

    -

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,5 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from '@remix-run/node';
+import { installGlobals } from "@remix-run/node";
 import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
