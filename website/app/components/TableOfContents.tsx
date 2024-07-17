ad8c9ae45c8f8eac12fc1f478f30fae248a45c67
Elliot Hesp <elliot.hesp@gmail.com>
2024-07-17 12:54:37 +0100

    lint

--- website/app/components/TableOfContents.tsx
+++ website/app/components/TableOfContents.tsx
@@ -4,65 +4,65 @@ import { usePageContext, useTabs } from "~/context";
 import { cn } from "~/utils";
 
 export function TableOfContents() {
-  const ctx = usePageContext();
-  const hasTabs = useTabs().length > 0;
-  const headings = ctx.bundle.headings;
-  const [activeId, setActiveId] = useState("");
+	const ctx = usePageContext();
+	const hasTabs = useTabs().length > 0;
+	const headings = ctx.bundle.headings;
+	const [activeId, setActiveId] = useState("");
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const sections = document.querySelectorAll("main div[data-section]");
-      let active = "";
+	useEffect(() => {
+		const handleScroll = () => {
+			const sections = document.querySelectorAll("main div[data-section]");
+			let active = "";
 
-      for (const section of sections) {
-        const span = section.querySelector("span:first-child");
-        if (span && span.getBoundingClientRect().y < 1) {
-          active = section.getAttribute("data-section") || "";
-        }
-      }
+			for (const section of sections) {
+				const span = section.querySelector("span:first-child");
+				if (span && span.getBoundingClientRect().y < 1) {
+					active = section.getAttribute("data-section") || "";
+				}
+			}
 
-      setActiveId(active);
-    };
+			setActiveId(active);
+		};
 
-    window.addEventListener("scroll", handleScroll);
+		window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  return (
-    <div
-      className={cn("sticky", {
-        "top-24": !hasTabs,
-        "top-32": hasTabs,
-      })}
-    >
-      <h3 className="flex items-center gap-2 opacity-75 font-display font-medium text-[15px] mb-2 tracking-wider">
-        <ListTreeIcon size={20} />
-        <span>On this page</span>
-      </h3>
-      <ul className="mt-4 space-y-2">
-        {headings.map((heading) => {
-          return (
-            <li
-              key={heading.id}
-              style={{
-                paddingLeft: `${((heading.rank || 2) - 2) * 0.75}rem`,
-              }}
-            >
-              <a
-                href={`#${heading.id}`}
-                className={cn("font-light text-[14px]", {
-                  "text-primary font-bold": activeId === heading.id,
-                })}
-              >
-                {heading.title}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+	return (
+		<div
+			className={cn("sticky", {
+				"top-24": !hasTabs,
+				"top-32": hasTabs,
+			})}
+		>
+			<h3 className="flex items-center gap-2 opacity-75 font-display font-medium text-[15px] mb-2 tracking-wider">
+				<ListTreeIcon size={20} />
+				<span>On this page</span>
+			</h3>
+			<ul className="mt-4 space-y-2">
+				{headings.map((heading) => {
+					return (
+						<li
+							key={heading.id}
+							style={{
+								paddingLeft: `${((heading.rank || 2) - 2) * 0.75}rem`,
+							}}
+						>
+							<a
+								href={`#${heading.id}`}
+								className={cn("font-light text-[14px]", {
+									"text-primary font-bold": activeId === heading.id,
+								})}
+							>
+								{heading.title}
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
--- website/app/components/mdx/InvalidComponent.tsx
+++ website/app/components/mdx/InvalidComponent.tsx
@@ -2,25 +2,25 @@ import type { ComponentProps } from "react";
 import { Error } from "./Callout";
 
 type InvalidComponentProps = ComponentProps<"div"> & {
-  // The name of the original component, provided by the bundler.
-  name?: string;
+	// The name of the original component, provided by the bundler.
+	name?: string;
 };
 
 export function InvalidComponent(props: InvalidComponentProps) {
-  if (!props.name) {
-    return null;
-  }
+	if (!props.name) {
+		return null;
+	}
 
-  return (
-    <Error>
-      Markdown content contains an invalid component declaration called{" "}
-      <code>
-        {"<"}
-        {props.name}
-        {" />"}
-      </code>
-      . To remove this error, either declare the component locally or remove the
-      declaration.
-    </Error>
-  );
+	return (
+		<Error>
+			Markdown content contains an invalid component declaration called{" "}
+			<code>
+				{"<"}
+				{props.name}
+				{" />"}
+			</code>
+			. To remove this error, either declare the component locally or remove the
+			declaration.
+		</Error>
+	);
 }
