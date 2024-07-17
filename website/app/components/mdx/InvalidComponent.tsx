7482b5958b12dbcbe0cb87fff44bc6b1b6d65eb9
Elliot Hesp <elliot.hesp@gmail.com>
2024-07-17 21:55:44 +0100

    fix: made InvalidComponent more prominent

--- website/app/components/mdx/InvalidComponent.tsx
+++ website/app/components/mdx/InvalidComponent.tsx
@@ -12,15 +12,17 @@ export function InvalidComponent(props: InvalidComponentProps) {
 	}
 
 	return (
-		<Error>
-			Markdown content contains an invalid component declaration called{" "}
-			<code>
-				{"<"}
-				{props.name}
-				{" />"}
-			</code>
-			. To remove this error, either declare the component locally or remove the
-			declaration.
-		</Error>
+		<div data-invalid-component={props.name} className="mb-3">
+			<div className="rounded-xl px-5 py-4 bg-red-500 text-white font-bold ring ring-red-500/30 ring-offset-2 ring-offset-background">
+				Markdown content contains an invalid component declaration called{" "}
+				<code className="text-white">
+					{"<"}
+					{props.name}
+					{" />"}
+				</code>
+				. To remove this error, either declare the component locally or remove
+				the declaration.
+			</div>
+		</div>
 	);
 }
