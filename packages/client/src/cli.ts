fc198f663e0253f52fba187037f06430e884cba4
Elliot Hesp <elliot.hesp@gmail.com>
2024-07-29 16:07:46 +0100

    update check output

--- packages/client/src/cli.ts
+++ packages/client/src/cli.ts
@@ -2,6 +2,7 @@
 
 import path from "node:path";
 import { program } from "commander";
+import chalk from "chalk";
 import { check } from "./check";
 
 // Setup the program
@@ -9,20 +10,42 @@ program.name("docs.page").version("0.1.0").description("docs.page CLI");
 
 // Add the check command
 program
-	.command("check")
-	.description("Check the validity of the docs")
-	// Path to the directory to check, defaults to current directory
-	.argument(
-		"[path]",
-		"Path to the relative directory to check. Defaults to the current directory.",
-	)
-	.action((input: unknown, o) => {
-		const relativePath = String(input || ".");
-		const absolutePath = path.resolve(relativePath);
+  .command("check")
+  .description("Check the validity of the docs")
+  // Path to the directory to check, defaults to current directory
+  .argument(
+    "[path]",
+    "Path to the relative directory to check. Defaults to the current directory."
+  )
+  .action((input: unknown, o) => {
+    const relativePath = String(input || ".");
+    const absolutePath = path.resolve(relativePath);
 
-		for (const result of check(absolutePath)) {
-			console.log(result);
-		}
-	});
+    let error = false;
+
+    for (const result of check(absolutePath)) {
+      // If there is an error, set the error flag
+      if (result.type === "error") {
+        error = true;
+      }
+
+      const message = [
+        `${chalk.blueBright(result.filePath)}`,
+        ":",
+        `${chalk.yellow(result.line)}`,
+        ":",
+        `${chalk.yellow(result.column)}`,
+        " - ",
+        result.type === "error" ? chalk.red("error") : chalk.yellow("warn"),
+        ": ",
+        result.message,
+      ];
+
+      console.log(message.join(""));
+    }
+
+    // If there is an error, exit with a non-zero code
+    process.exit(error ? 1 : 0);
+  });
 
 program.parse(process.argv);
