017ca85e11d551816564b70c33088910faa0c31e
Elliot Hesp <elliot.hesp@gmail.com>
2024-08-28 14:56:50 +0100

    -

--- website/app/root.tsx
+++ website/app/root.tsx
@@ -51,7 +51,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   const data = useRouteLoaderData<typeof loader>("root");
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
