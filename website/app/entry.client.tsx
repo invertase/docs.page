53211672643b979f4e3ed0d080e4f07aff10e3d7
Elliot Hesp <elliot.hesp@gmail.com>
2024-08-28 15:05:56 +0100

    -

--- website/app/entry.client.tsx
+++ website/app/entry.client.tsx
@@ -15,16 +15,23 @@ const DOMAINS = ["docs.page", "staging.docs.page"];
 if (import.meta.env.PROD) {
   const hostname = window.location.hostname;
 
+  if (hostname === "invertase.docs.page") {
+    console.log('TESTING', window.__remixContext);
+    window.__remixContext.basename = "/invertase/supabase-js~docs-test";
+    window.__remixContext.url = "/memes";
+    console.log('TESTING DONE', window.__remixContext);
+  }
+
   // If we're not on one of the domains above, then we need to modify remix context so
   // that the app is aware of the correct base URL.
-  if (
+  else if (
     (!DOMAINS.includes(hostname) || hostname.includes(".docs.page")) &&
     window.__docsPage
   ) {
     const { owner, repository } = window.__docsPage;
     const basename = `/${owner}/${repository}`;
 
-    console.log('Remix Context', window.__remixContext);
+    console.log("Remix Context", window.__remixContext);
 
     // Set the base name to the owner and repository (e.g. `/invertase/docs.page`).
     window.__remixContext.basename = basename;
