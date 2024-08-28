8a21c717ff3c5658dea39c3c5fbbb584545ffe53
Elliot Hesp <elliot.hesp@gmail.com>
2024-08-28 15:29:54 +0100

    -

--- website/app/entry.client.tsx
+++ website/app/entry.client.tsx
@@ -15,38 +15,32 @@ const DOMAINS = ["docs.page", "staging.docs.page"];
 if (import.meta.env.PROD && window.__docsPage) {
   const hostname = window.location.hostname;
 
-  // // Check if the current hostname is a vanity domain (e.g. `:org.docs.page`).
-  // const isVanityDomain = hostname.includes(".docs.page") && !DOMAINS.includes(hostname);
+  // Check if the current hostname is a vanity domain (e.g. `:org.docs.page`).
+  const isVanityDomain =
+    hostname.includes(".docs.page") && !DOMAINS.includes(hostname);
 
-  // // It's a custom domain if it's not a vanity domain and it's not one of the base domains.
-  // const isCustomDomain = !isVanityDomain && !DOMAINS.includes(hostname);
+  // It's a custom domain if it's not a vanity domain and it's not one of the base domains.
+  const isCustomDomain = !isVanityDomain && !DOMAINS.includes(hostname);
 
-  // const { owner, repository } = window.__docsPage;
+  const { owner, repository } = window.__docsPage;
 
-  // if (isVanityDomain) {
-  //   console.log("Vanity domain detected: ", hostname);
-  //   window.__remixContext.basename = "/";
-  //   let url = `/${repository}`;
+  // A vanity domain is a rewrite request.
+  if (isVanityDomain) {
+    window.__remixContext.basename = "/";
+    // Remove the owner from the URL (since it's now part of the hostname).
+    window.__remixContext.url = window.__remixContext.url.replace(
+      `/${owner}`,
+      ""
+    );
 
-  // }
-
-  if (hostname === "invertase.docs.page") {
-    console.log('VANITY DOMAIN', window.__remixContext);
-    // window.__remixContext.basename = "/";
-    // window.__remixContext.url = "/supabase-js~docs-test/memes";
-    // console.log('TESTING DONE', window.__remixContext);
+    console.log("Rewriting context for vanity domain: ", {
+      basename: window.__remixContext.basename,
+      url: window.__remixContext.url,
+    });
   }
-
-  // If we're not on one of the domains above, then we need to modify remix context so
-  // that the app is aware of the correct base URL.
-  else if (
-    (!DOMAINS.includes(hostname) || hostname.includes(".docs.page")) &&
-    window.__docsPage
-  ) {
-    const { owner, repository } = window.__docsPage;
+  // A custom domain is a proxy request.
+  else if (isCustomDomain) {
     const basename = `/${owner}/${repository}`;
-
-    console.log("Remix Context", window.__remixContext);
 
     // Set the base name to the owner and repository (e.g. `/invertase/docs.page`).
     window.__remixContext.basename = basename;
@@ -55,9 +49,8 @@ if (import.meta.env.PROD && window.__docsPage) {
     // For example: `/invertase/docs.page/configuration` -> `/configuration`.
     window.__remixContext.url = window.__remixContext.url.replace(basename, "");
 
-    console.log("Rewriting remix context for custom domain: ", {
-      hostname,
-      basename,
+    console.log("Rewriting context for custom domain: ", {
+      basename: window.__remixContext.basename,
       url: window.__remixContext.url,
     });
   }
