ab3f0300a0800dbbcd021b516faa551ba0fdd4f0
Elliot Hesp <elliot.hesp@gmail.com>
2024-08-28 14:49:13 +0100

    -

--- website/app/entry.client.tsx
+++ website/app/entry.client.tsx
@@ -12,31 +12,34 @@ declare global {
 // A list of base domains which can run this app in production.
 const DOMAINS = ["docs.page", "staging.docs.page"];
 
-startTransition(() => {
-  if (import.meta.env.PROD) {
-    const hostname = window.location.hostname;
+if (import.meta.env.PROD) {
+  const hostname = window.location.hostname;
 
-    // If we're not on one of the domains above, then we need to modify remix context so
-    // that the app is aware of the correct base URL.
-    if (
-      (!DOMAINS.includes(hostname) || hostname.includes(".docs.page")) &&
-      window.__docsPage
-    ) {
-      const { owner, repository } = window.__docsPage;
-      const basename = `/${owner}/${repository}`;
+  // If we're not on one of the domains above, then we need to modify remix context so
+  // that the app is aware of the correct base URL.
+  if (
+    (!DOMAINS.includes(hostname) || hostname.includes(".docs.page")) &&
+    window.__docsPage
+  ) {
+    const { owner, repository } = window.__docsPage;
+    const basename = `/${owner}/${repository}`;
 
-      // Set the base name to the owner and repository (e.g. `/invertase/docs.page`).
-      window.__remixContext.basename = basename;
+    // Set the base name to the owner and repository (e.g. `/invertase/docs.page`).
+    window.__remixContext.basename = basename;
 
-      // Replace the URL which includes the repository with the correct URL.
-      // For example: `/invertase/docs.page/configuration` -> `/configuration`.
-      window.__remixContext.url = window.__remixContext.url.replace(
-        basename,
-        ""
-      );
-    }
+    // Replace the URL which includes the repository with the correct URL.
+    // For example: `/invertase/docs.page/configuration` -> `/configuration`.
+    window.__remixContext.url = window.__remixContext.url.replace(basename, "");
+
+    console.log("Rewriting remix context for custom domain: ", {
+      hostname,
+      basename,
+      url: window.__remixContext.url,
+    });
   }
+}
 
+startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
