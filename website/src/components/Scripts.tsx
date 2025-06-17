9a05df67e4e30796e7f366d9d2bc2d86005447c1
Elliot Hesp <elliot.hesp@gmail.com>
2025-06-17 13:02:14 +0100

    fix: Update how GTM and GA is initialized

--- website/src/components/Scripts.tsx
+++ website/src/components/Scripts.tsx
@@ -4,7 +4,7 @@ import { getEnvironment } from "~/env";
 
 export function Scripts() {
   const ctx = usePageContext();
-  const scripts = usePageContext().bundle.config.scripts;
+  const scripts = ctx.bundle.config.scripts;
 
   // We don't want to load the scripts in preview mode or in preview environments.
   if (ctx.preview) {
@@ -13,29 +13,53 @@ export function Scripts() {
 
   return (
     <>
+      {/* Google Tag Manager */}
       {!!scripts?.googleTagManager && (
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${scripts.googleTagManager}`}
-        />
-      )}
-      {!!scripts?.googleAnalytics && (
         <>
           <Script
-            async
+            id="gtm-script"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${scripts.googleTagManager}');
+              `,
+            }}
+          />
+          <noscript>
+            <iframe
+              title="Google Tag Manager"
+              src={`https://www.googletagmanager.com/ns.html?id=${scripts.googleTagManager}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        </>
+      )}
+
+      {/* Google Analytics - Only load directly if not using GTM */}
+      {!!scripts?.googleAnalytics && !scripts?.googleTagManager && (
+        <>
+          <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${scripts.googleAnalytics}`}
           />
           <Script
-            async
+            id="ga-script"
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', '${scripts.googleAnalytics}');
-          `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${scripts.googleAnalytics}');
+              `,
             }}
           />
         </>
       )}
+
       {"domain" in ctx && !!ctx.domain && !!scripts?.plausible && (
         <Script
           async
@@ -43,7 +67,7 @@ export function Scripts() {
           data-domain={ctx.domain}
           src={
             typeof scripts.plausible === "boolean"
-              ? "https://plausible.io/js/plausible.js"
+              ? "https://plausible.io/js/script.js"
               : scripts.plausible
           }
         />
