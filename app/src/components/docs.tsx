8b6a8daba565ff6355b8a95f2bf73b2ae28ee6d2
Elliot Hesp <elliot.hesp@gmail.com>
2026-05-14 19:12:17 +0100

    feat: add nprogress

--- app/src/components/docs.tsx
+++ app/src/components/docs.tsx
@@ -7,41 +7,49 @@ import { TableOfContents } from "./table-of-contents";
 import { ResizablePanel, ResizablePanelGroup } from "./ui/resizable";
 import { PageMetadata } from "./page-metadata";
 import { Footer } from "./footer";
+import dynamic from "next/dynamic";
+
+const NProgress = dynamic(() => import("@/components/nprogress"), {
+  ssr: false,
+});
 
 export function Docs() {
   return (
-    <AgentPanelProvider>
-      <div className="flex w-full flex-1 flex-row">
-        <ResizablePanelGroup
-          orientation="horizontal"
-          className="overflow-visible!"
-        >
-          <ResizablePanel
-            id="docs-content-panel"
-            className="flex min-w-0 flex-1 flex-col overflow-visible!"
+    <>
+      <NProgress />
+      <AgentPanelProvider>
+        <div className="flex w-full flex-1 flex-row">
+          <ResizablePanelGroup
+            orientation="horizontal"
+            className="overflow-visible!"
           >
-            <Header />
-            <div className="relative mx-auto flex min-h-0 w-full max-w-8xl flex-1 flex-row">
-              <Navigation />
-              <div className="min-w-0 flex-1 flex @container">
-                <div className="min-w-0 flex-1 pt-8 ps-6 pe-6 @3xl:ps-14 @3xl:pe-14 @5xl:ps-16 @5xl:pe-16">
-                  <div className="space-y-12">
-                    <PageMetadata />
-                    <Prose />
-                    <Footer />
+            <ResizablePanel
+              id="docs-content-panel"
+              className="flex min-w-0 flex-1 flex-col overflow-visible!"
+            >
+              <Header />
+              <div className="relative mx-auto flex min-h-0 w-full max-w-8xl flex-1 flex-row">
+                <Navigation />
+                <div className="min-w-0 flex-1 flex @container">
+                  <div className="min-w-0 flex-1 pt-8 ps-6 pe-6 @3xl:ps-14 @3xl:pe-14 @5xl:ps-16 @5xl:pe-16">
+                    <div className="space-y-12">
+                      <PageMetadata />
+                      <Prose />
+                      <Footer />
+                    </div>
                   </div>
-                </div>
-                <div className="relative shrink-0 overflow-x-clip opacity-0 w-0 @3xl:w-68 @3xl:opacity-100 transition-[width,opacity] duration-200 ease-out">
-                  <div className="w-68 h-full">
-                    <TableOfContents />
+                  <div className="relative shrink-0 overflow-x-clip opacity-0 w-0 @3xl:w-68 @3xl:opacity-100 transition-[width,opacity] duration-200 ease-out">
+                    <div className="w-68 h-full">
+                      <TableOfContents />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </ResizablePanel>
-          <AgentPanel />
-        </ResizablePanelGroup>
-      </div>
-    </AgentPanelProvider>
+            </ResizablePanel>
+            <AgentPanel />
+          </ResizablePanelGroup>
+        </div>
+      </AgentPanelProvider>
+    </>
   );
 }
---
+++ app/src/components/nprogress.tsx
@@ -0,0 +1,35 @@
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+let timer: ReturnType<typeof setTimeout>;
+let state: string;
+const delay = 250;
+
+NProgress.configure({ showSpinner: false });
+
+function load() {
+  if (state === "loading") {
+    return;
+  }
+
+  state = "loading";
+
+  timer = setTimeout(() => {
+    NProgress.start();
+  }, delay); // only show progress bar if it takes longer than the delay
+}
+
+function stop() {
+  state = "stop";
+  clearTimeout(timer);
+  NProgress.done();
+}
+
+Router.events.on("routeChangeStart", load);
+Router.events.on("routeChangeComplete", stop);
+Router.events.on("routeChangeError", stop);
+
+export default function () {
+  return null;
+}
