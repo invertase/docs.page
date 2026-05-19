4ceae542a6eac5a79b7aea24f4d012ae3d4b8bf0
Elliot Hesp <elliot.hesp@gmail.com>
2026-05-19 13:46:45 +0100

    feat: add footer utm tags

--- app/src/components/footer.tsx
+++ app/src/components/footer.tsx
@@ -30,6 +30,11 @@ export function Footer() {
   const ctx = useDocPageContext();
   const socials = Object.entries(ctx.bundle.config?.social || {});
 
+  const params = new URLSearchParams();
+  params.set('utm_source', `${ctx.route.owner}-${ctx.route.repository}`.toLowerCase());
+  params.set('utm_medium', 'footer');
+  params.set('utm_campaign', 'docs.page');
+
   // Sorting here ensures that the socials are always displayed in the same order,
   // on client and server side.
   const sorted = socials.sort(([a], [b]) => a.localeCompare(b));
@@ -38,14 +43,14 @@ export function Footer() {
     <footer className="py-12 border-t flex text-muted-foreground">
       <div className="font-mono text-sm flex items-center gap-1.5">
         <a
-          href="https://docs.page"
+          href={`https://docs.page?${params.toString()}`}
           className="underline underline-offset-5 hover:text-foreground"
         >
           docs.page
         </a>
         <span className="text-muted-foreground">by</span>
         <a
-          href="https://invertase.io"
+          href={`https://invertase.io?${params.toString()}`}
           className="underline underline-offset-5 hover:text-foreground"
         >
           invertase.io
