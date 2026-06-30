41f37b7a7a851f3a61e1b45c077a5375741ed369
Elliot Hesp <elliot.hesp@gmail.com>
2026-06-30 14:25:01 +0100

    feat: add data-docs-page marker

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="h-full">
+    <Html lang="en" data-docs-page className="h-full">
       <Head>
         <link href="/_docs.page/fa/fontawesome.min.css" rel="stylesheet" />
         <link href="/_docs.page/fa/brands.min.css" rel="stylesheet" />
